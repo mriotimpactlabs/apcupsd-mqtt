ae65160b3fcacb4701d7a5fc64f65a25f8f1c77f
mriotimpactlabs <mr@iotimpactlabs.com>
2017-05-18 14:45:39 -0400

    Update apcupsd-mqtt.sh

--- apcupsd-mqtt.sh
+++ apcupsd-mqtt.sh
@@ -22,35 +22,3 @@ payload="{\"Status\":\""$ups_status"\",\"LineVoltage\":\""$ups_line_voltage"V\",
 
 #Publishes UPS data via MQTT.
 mosquitto_pub -h iot.eclipse.org -m $payload -t $mqtt_topic
-
-
-:'
-
-Script description:
-
-Instructions
-
-1.) Install dependency software.
-
-# $ apt-get update && apt-get install -y apcupsd mosquitto-clients
-
-2.) Give permissions.
-
- $ chmod +x apcupsd-mqtt.sh
-
-3.) Add this to cronjobs.
-
-4.) Modify /etc/apcupsd/apcupsd: Change IsConfigured=yes
-5.) Modify /etc/apcupsd/apcupsd.conf
-
-
-6.) sudo service apcupsd start
-
-#Things to be considered:
-#Topic Name
-#Different Models will have different parameters dispensed.  This needs to be addressed.
-#Build Script which has a list of configuration parameters in a single file.
-
-    
-
-'
