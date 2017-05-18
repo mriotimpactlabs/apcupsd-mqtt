44415bc84e9e3aa8aa5c8affda032431f50031c1
mriotimpactlabs <mr@iotimpactlabs.com>
2017-05-18 14:40:04 -0400

    Update apcupsd-mqtt.sh

--- apcupsd-mqtt.sh
+++ apcupsd-mqtt.sh
@@ -40,4 +40,17 @@ Instructions
 
 3.) Add this to cronjobs.
 
+4.) Modify /etc/apcupsd/apcupsd: Change IsConfigured=yes
+5.) Modify /etc/apcupsd/apcupsd.conf
+
+
+6.) sudo service apcupsd start
+
+#Things to be considered:
+#Topic Name
+#Different Models will have different parameters dispensed.  This needs to be addressed.
+#Build Script which has a list of configuration parameters in a single file.
+
+    
+
 '
